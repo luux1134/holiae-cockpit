6ad2b47e521aeb9d221a10cc52e25a60cd443c57
luux1134 <gueperoux.lucas@gmail.com>
2026-04-26 23:15:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "holiae-cockpit-v10";
+const CACHE_NAME = "holiae-cockpit-v11";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
