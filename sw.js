88556ed9fa7967833f30ec7d11e3071bf44e141e
luux1134 <gueperoux.lucas@gmail.com>
2026-04-27 15:00:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "holiae-cockpit-v22";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "holiae-cockpit-v23";
+const ASSETS = ["./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
@@ -15,6 +15,20 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   const url = new URL(event.request.url);
+
+  if (event.request.mode === "navigate" || url.pathname.endsWith("/") || url.pathname.endsWith("/index.html")) {
+    event.respondWith(
+      fetch(new Request(event.request, { cache: "no-store" }))
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html").then(cached => cached || caches.match("./")))
+    );
+    return;
+  }
+
   if (url.hostname.includes("sheets.googleapis.com")) {
     event.respondWith(
       fetch(event.request).catch(() => caches.match(event.request))
